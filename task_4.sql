8f3a023102dabb97525ea319784f2292a182926c
Samkelo Gadinabokao <siamkelegadinabokao@gmail.com>
2026-02-09 14:10:58 +0200

    Create task_4.sql for Books table description
    Add SQL script to show full description of Books table

---
+++ task_4.sql
@@ -0,0 +1,9 @@
+-- task_4.sql
+-- Show full description of the Books table in alx_book_store
+
+USE alx_book_store;
+
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
