86b376f7d3f5bf9801444f8fe8a3d826d39ec60d
Samkelo Gadinabokao <siamkelegadinabokao@gmail.com>
2026-02-09 14:27:13 +0200

    Add multiple customer records to Customers table
    Inserted multiple customer records into the Customers table in alx_book_store.

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,9 @@
--- Insert multiple customer records into the customers table
-INSERT INTO customer (customer_id, customer_name, email, address)
+-- task_6.sql
+-- Insert multiple rows into the Customers table in alx_book_store
+
+USE alx_book_store;
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES 
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
